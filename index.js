c7df410c5813c7fc6d0ff8206a0eadca94386eaa
Mubashir Kabir <97.m.kabir@gmail.com>
2022-11-24 21:44:22 +0600

    database connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //required packages
 const express = require("express");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -15,6 +16,21 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Server is Up...");
 });
+
+//mongodb connection
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.erzixne.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+//database connection function
+const dbConnection = async () => {
+  try {
+    await client.connect();
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Database connection error", error.name, error.message);
+  }
+};
+dbConnection();
 
 //server listener
 app.listen(port, () => {
