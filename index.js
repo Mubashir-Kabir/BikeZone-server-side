683639b3ef3fc2c98e40456a74e68470bed7874f
Mubashir Kabir <97.m.kabir@gmail.com>
2022-11-24 21:21:49 +0600

    initializing

---
+++ index.js
@@ -0,0 +1,22 @@
+//required packages
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+//root route
+app.get("/", (req, res) => {
+  res.send("Server is Up...");
+});
+
+//server listener
+app.listen(port, () => {
+  console.log(`server is listening at port ${port}`);
+});
